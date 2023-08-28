91d8a21827442ca1f770e0ade961dc5360add72e
forhad30 <forhadchariani30@gmail.com>
2023-08-28 11:03:49 +0600

    first commit

---
+++ problem.js
@@ -0,0 +1,265 @@
+// const title = {
+//     name:'forhad',
+//     id:4548541,
+//     city:'Dhaka'
+// }
+// console.log(title)
+
+// let title = {
+//     name:'nasir',
+//     address:'nasirkut',
+//     city:'chariany malapara'
+
+// }
+// console.log(title)
+
+// const number = [4, 5, 56, 77, 12450];
+
+// const doubled = [];
+// for (const num of number){
+//     const double = num *2
+//     doubled.push(double)
+// }
+// console.log(doubled)
+
+
+// const number = [41, 52, 56];
+// const numbers = number.map(num => num * 5);
+// console.log(numbers)
+
+
+// const number = [4, 8, 3, 6, 9, ]
+
+// const doubled = []
+// for(const num of number){
+//     const double = num * 2
+//     doubled.push(double);
+// }
+// console.log(doubled)
+
+// const number = [5, 2, 4, 6];
+
+// const doubled = [];
+// for(const num of number){
+// const double = num * 3
+// doubled.push(double)
+// }
+// console.log(doubled)
+
+
+
+
+// const number =[4, 5, 7, 8, 9]
+// const doubled = []
+// for(const num of number){
+//     const doubld = num *3
+//     doubled.push(doubld)
+// }
+// console.log(doubled)
+
+
+//Reversed function  0000
+
+// function reversedString(number) {
+//     let reversed = '';
+//     for (let i = number.length -1; i>=0; i--){
+//         const element = number[i];
+//         reversed = reversed + element
+//         console.log(element, reversed)
+      
+//     }    
+//     return reversed;
+// }
+
+// const mySting = 'Dhaka'
+// const reversed = reversedString(mySting)
+// console.log(reversed)
+
+
+// function reversedString(number) {
+//     let reversed = '';
+//     for (let i = number.length -1; i>0; i--) {
+//         const element= number[i];
+//         reversed = reversed + element;
+//         console.log(element,reversed)  
+//     }
+//     return reversed;
+// }
+// const mySting = 'forhad Ahmed';
+// const reversed = reversedString(mySting)
+// console.log(reversed)
+
+
+// function reversedString(number) {
+//     let reversed = '';
+//     for (let i = number.length-2; i>0; i--){
+//         const element = number[i];
+//         reversed = reversed + element;
+//         console.log(element,reversed)
+        
+//     }
+//     return reversed;
+// }
+// const mySting = 'forhad ahmed'
+// const stisntme = reversedString(mySting)
+// console.log(stisntme)
+
+
+
+// function reversedString(number) {
+//     let reversed = '';
+//     for (let i = number.length-2; i>0; i--) {
+//         const element = number[i];
+//         reversed = reversed + element;
+//         console.log(element,reversed);
+        
+//     }
+//     return reversed;
+// }
+// const mySting = 'Amar sunar bangla ami tumai balobi';
+// const amader = reversedString(mySting)
+// console.log( 'I ma your student good boy',amader)
+
+
+
+// function reversedString(number) {
+//     let reversed = '';
+//     for (let i = number.length-1; i>0; i--) {
+//         const element = number[i];
+//         reversed = reversed + element;
+//         console.log(element, reversed)
+        
+//     }
+//     return reversed
+// }
+// const Mystring = 'forhad chariany'
+// const reversed = reversedString(Mystring)
+// console.log(reversed)
+
+// Revered string 000000
+
+
+
+// const a = 4;
+// const b = 'forhad ahmed';
+// const student =  {id:12, class: 12}
+// const ages =  [45,  77, 88]
+// console.log(typeof a , typeof b , typeof ages, typeof student)
+
+// let x = 5;
+// let y = x;
+// console.log(x,y)
+// y = 7;
+// console.log(x,y)
+// let p ={job: 'web doveloper'}
+// let q = p;
+// q.job = 'font end web developer'
+// console.log(q,p)
+
+
+//32.2 
+// function noNegative(a,b) {
+//     if(a<0||b<0){
+//         return;
+
+//     }
+//     return a+b
+// }
+// const total = noNegative(2,7);
+// console.log(total) 
+// const frfth = {id:222, name:'forahd ahmed' , ages: 21}
+// console.log(frfth)
+
+
+//32.3 
+// let x = [5.4]
+// const y = 2;
+// if (x) {
+//     console.log('inside the loop ')
+// }
+// else{
+//     console.log('vlue outside the loop')
+// }
+
+// const z = null;
+// if (!z) {
+//     console.log('thear is a vari')
+// }
+// const i = {class: 8}
+// if (!!x) {
+//     console.log('forhad ahmed')
+    
+// }
+
+
+//32.4
+// const first = 2; []
+// const second = 2; []
+// if (first === second) {
+//     console.log('inside the loop')
+// }
+// else{
+//     console.log('outside the loop ')
+// }
+
+// 32.5
+// function add(a,b) {
+//     const total = a + b
+//     if (b>10) {
+//         const sum = 45 + a + b;
+
+
+//     }   
+//     else{
+//         const sum = 5 + a + b ;
+//         var current = sum;
+//     }
+//      console.log(current);
+//     return total;
+   
+// }
+
+// //  console.log(a,b);
+// //  console.log(total);
+// //  add(9,10)
+
+//  print5();
+//  for (let i = 0; i <=10;  i++) {
+//   console.log(i)
+//   console.log('outside', i)
+    
+//  }
+// console.log('outside', 5)
+//  function print5() {
+//     console.log('inside the Print',5)
+//  }
+
+
+// let num1 = 5;
+// let num2 = 9;
+
+// function multiple(a,b) {
+//     a = 27;
+//     const result= a*b;
+//     return result;
+
+// }
+
+// console.log(num1,num2)
+// multiple(num1,num2);
+// console.log(num1)
+
+
+// let student1 = {name:'forhad',job:'web devoloper', }
+// let student2 = {name:'ahmed', nayka:'anika'};
+// function makevie(couple1, couple2) {
+//     makevie(student1, student2);
+// }
+// console.log(student1, student2)
+
+
+// let student1 ={name:'forhad',job:'web devoleper', age:21}
+// let student2 = {name:'ahmed', city: 'Dhaka'}
+
+// console.log(student1,student2)
+
